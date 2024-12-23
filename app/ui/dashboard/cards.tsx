16474a6110c376432d1a021cde61d5dcbc710c74
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2024-12-23 10:12:47 -0500

    crud functionality on invoices

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -6,12 +6,6 @@ import {
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
-const {
-  numberOfInvoices,
-  numberOfCustomers,
-  totalPaidInvoices,
-  totalPendingInvoices,
-} = await fetchCardData();
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -21,9 +15,15 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
